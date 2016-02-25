809092d66f4d81dd1f1b7d748339cdb3cca50c29
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2016-02-25 18:16:50 +0100

    Add UserNotifier mailer class

---
+++ app/mailers/user_notifier.rb
@@ -0,0 +1,20 @@
+class UserNotifier < ApplicationMailer
+
+	def friend_requested(user_friendship_id)
+		user_friendship = UserFriendship.find(user_friendship_id)
+		@user = user_friendship.user
+		@friend = user_friendship.friend
+
+		mail to: @friend.email,
+				subject: "#{@user.full_name} wants to be friends on Embook"
+	end
+
+	def friend_request_accepted(user_friendship_id)
+		user_friendship = UserFriendship.find(user_friendship_id)
+		@user = user_friendship.user
+		@friend = user_friendship.friend
+
+		mail to: @friend.email,
+				subject: "#{@user.full_name} has accepted your friend request."
+	end
+end
