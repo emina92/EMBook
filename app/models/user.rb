80da78e6c78f4e72ae8cd1053b61052133bb0aae
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2016-02-26 12:59:45 +0100

    Add has_many associations pending_user_friendship and pending_friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,12 @@ class User < ActiveRecord::Base
 
   has_many :statuses
   has_many :user_friendships
-  has_many :friends, -> { where(user_friendship: {state: 'accepted'})}, :through => :user_friendships
-  
+  has_many :friends, -> { where(user_friendships: {state: 'accepted'})}, :through => :user_friendships
+  has_many :pending_user_friendships, -> { where(user_friendships: {state: 'pending'})}, class_name: 'UserFriendship', foreign_key: :user_id
+  has_many :pending_friends, through: :pending_user_friendships, source: :friend
+
+
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :profile_name, presence: true, uniqueness: true,
