3906a199ec047bc98a09bb5c5b4d7e57dae21c45
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2016-02-29 14:24:13 +0100

    Add method perform_attachment_removal and validation for attachment

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,15 @@
 class Document < ActiveRecord::Base
 	has_attached_file :file
+	validates_attachment_content_type :file, content_type: /\Aimage\/.*\Z/
+
+	attr_accessor :remove_attachment
+
+	before_save :perform_attachment_removal
+	
+	def perform_attachment_removal
+		if remove_attachment == "1" && !file.dirty?
+			self.file = nil
+		end
+	end
+
 end
