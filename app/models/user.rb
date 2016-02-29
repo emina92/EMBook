4f404f8528296a9ce5b70d9421cfbd5f0131446d
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2016-02-29 11:57:55 +0100

    Add has_many associations for requested and blocked friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,15 @@ class User < ActiveRecord::Base
   has_many :statuses
   has_many :user_friendships
   has_many :friends, -> { where(user_friendships: {state: 'accepted'})}, :through => :user_friendships
+  
   has_many :pending_user_friendships, -> { where(user_friendships: {state: 'pending'})}, class_name: 'UserFriendship', foreign_key: :user_id
   has_many :pending_friends, through: :pending_user_friendships, source: :friend
 
+  has_many :requested_user_friendships, -> { where(user_friendships: {state: 'requested'})}, class_name: 'UserFriendship', foreign_key: :user_id
+  has_many :requested_friends, through: :requested_user_friendships, source: :friend
+
+  has_many :blocked_user_friendships, -> { where(user_friendships: {state: 'blocked'})}, class_name: 'UserFriendship', foreign_key: :user_id
+  has_many :blocked_friends, through: :blocked_user_friendships, source: :friend
 
 
   validates :first_name, presence: true
@@ -19,6 +25,7 @@ class User < ActiveRecord::Base
     	with:  /\A[a-zA-Z0-9_-]+\z/,
     	message: "Must be formatted correctly."
   	}
+
   def full_name
   	first_name + " " + last_name
   end
@@ -32,5 +39,9 @@ class User < ActiveRecord::Base
     downcased_email = stripped_email.downcase
     hash = Digest::MD5.hexdigest(downcased_email)
     "http:://gravatar.com/avatar/#{hash}"
+  end
+
+  def has_blocked(other_user)
+    blocked_friends.include?(other_user)
   end
 end
