1f6c5b88648aafebd819f081e650425b7583b7bf
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2016-03-09 20:29:16 +0100

    Add updates and notifications

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,9 @@
 class ActivitiesController < ApplicationController
+	respond_to :html, :json
   def index
   	params[:page] ||= 1
   	friend_ids = current_user.friends.map(&:id)
   	@activities = Activity.where("user_id in (?)", friend_ids.push(current_user.id)).order("created_at desc").all.page(params[:page])
+  	respond_with @activities
   end
 end
